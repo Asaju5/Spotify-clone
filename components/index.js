4f0eca3eb1ea1aa1d1015903839a5b6e4c5b5938
Bamidele <bamidelesimeon05@gmail.com>
2022-03-19 23:19:32 -0700

    changed

---
+++ components/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Skills() {
+  return (
+    <div className='text-2xl font-semibold p-2 flex flex-wrap justify-center mb-20 ml-4'>
+        HTML, CSS, Javascript, Reactjs, Nextjs, Nodejs, Tailwindcss, MongoDB, Redux, Git.
+    </div>
+  )
+}
+
+export default Skills
